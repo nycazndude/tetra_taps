940135cfb288d0c1bb57788ec0ea442aaf00188f
Jason <yimjason00@gmail.com>
2015-02-04 18:27:27 -0500

    ui/ux added lights and level stat

--- js/app.js
+++ js/app.js
@@ -3,19 +3,23 @@ angular.module("tap", [])
 	var self = this;
 	var timer = 500;
 	var speed = 1000;
-	var turn = 1; 
 	var count = 0;
-	var wait = speed * (turn+2);
-	var win_text1  = ["Correct!",          "Easy!",       "Yep!",      "No Sweat!"     ];
-	var win_text2  = ["Right On!",         "Keep It Up!", "Nice!",     "Good!"         ];
-	var win_text3  = ["On Fire!",          "You Got It!", "Awesome!",  "Cool!"         ];
-	var win_text4  = ["Oh Yeah Baby!",     "Amazing!",    "Wow!",      "You're Good"   ];
-	var win_text5  = ["Are You Cheating?", "OWNAGE!",     "Wrecking!", "Terrific!"     ];
-	var lose_text1 = ["Sigh..",            "T.T",         "No Good",   "Not Even Close"];
-	var lose_text2 = ["Oops!",             "Wrong One",   "Sorry!",    "Nope"          ];
-	var lose_text3 = ["Maybe Next Time?",  "Oh No...",    "Bummer!",   "Ay Yaah!"      ];	
+	var wait = speed * (self.level+2);
+	var win_text1  = ["Correct!",          "Easy!",       "Yep!",      "No Sweat!",      "Rock On!", "Yay!"        ];
+	var win_text2  = ["Right On!",         "Bingo!",      "Nice!",     "Good!",          "Yes!",     "Cool"      ];
+	var win_text3  = ["On Fire!",          "You Got It!", "Awesome!",  "Keep It Up!",    "Sweet!",   "Fantastic!"];
+	var win_text4  = ["Oh Yeah Baby!",     "Amazing!",    "Wow!",      "You're Good",    "A++",      "Brilliant!"];
+	var win_text5  = ["Are You Cheating?", "OWNAGE!",     "Wrecking!", "Terrific!",      "You're The Bomb!"      ];
+	var lose_text1 = ["Sigh..",            "T.T",         "No Good",   "Not Even Close", "Try Harder!"           ];
+	var lose_text2 = ["Oops!",             "Wrong One",   "Sorry!",    "Nope",           "Yikes!"                ];
+	var lose_text3 = ["Maybe Next Time?",  "Oopsies!",    "Oh No...",  "Bummer!",        "Ay Yaah!",    "Close!" ];	
+	var lose_text4 = ["You Were Amazing!", "You Had It!", "So Close!", "Ouch!",          "Unlucky!"];
 	self.score = 0;
+	self.level = 0;
+	self.light = false;
+	self.score_clicked = false;
 	self.tetra_choices = [];
+	self.score_board = [];
 	self.red_click = false;
 	self.blue_click = false;
 	self.green_click = false;
@@ -30,10 +34,10 @@ angular.module("tap", [])
 	self.reset = function(){ 
 		timer = 500;
 		speed = 1000;
-		turn = 1; 
 		count = 0;
-		wait = speed * (turn+2);
+		wait = speed * (self.level+2);
 		self.score = 0;
+		self.level = 0;
 		self.tetra_choices = [];
 		self.red_click = false;
 		self.blue_click = false;
@@ -75,8 +79,9 @@ angular.module("tap", [])
 
 	self.tetrasTurn = function(){
 		self.player_turn = false;
+		self.level++;
 		timer = speed/2;
-		wait = speed * (turn+2); // waits for tetra to finish clicks
+		wait = speed * (self.level+2); // waits for tetra to finish clicks
 		self.message = "Tetra's Turn";
 		//timeout function gives player some time before tetra starts
 		$timeout(function(){
@@ -88,7 +93,7 @@ angular.module("tap", [])
 	self.classicMode = function(){
 		var i = 0;
 		var stop = $interval(function(){
-			if(i >= turn-1){$interval.cancel(stop);}
+			if(i >= self.level-1){$interval.cancel(stop);}
 			if(i < self.tetra_choices.length && self.tetra_choices.length != 0){
 				self.colorClicked(self.tetra_choices[i]);
 				i++;
@@ -99,7 +104,7 @@ angular.module("tap", [])
 	self.tetraMode = function(){
 		var i = 0;
 		var stop = $interval(function(){
-			if(i >= turn-1){$interval.cancel(stop);}
+			if(i >= self.level-1){$interval.cancel(stop);}
 			self.tetraPicks();
 			i++;
 		}, speed); 
@@ -123,7 +128,6 @@ angular.module("tap", [])
 			//check to see if player clears round
 			if(count >= self.tetra_choices.length){
 				count = 0;
-				turn++;
 				if(self.tetra_mode == true){self.tetra_choices = [];}
 				$timeout(function(){
 					speed -= 50;
@@ -141,41 +145,73 @@ angular.module("tap", [])
 	}
 	self.winMessage = function(){
 		switch(true){
-			case (count >= 1  && count <  4):self.message = win_text1[self.randomNumber(4)];break;
-			case (count >= 4  && count <  7):self.message = win_text2[self.randomNumber(4)];break;
-			case (count >= 7  && count < 10):self.message = win_text3[self.randomNumber(4)];break;
-			case (count >= 10 && count < 15):self.message = win_text4[self.randomNumber(4)];break;
-			case (count >= 15 && count < 20):self.message = win_text5[self.randomNumber(4)];break;
-			case (count >= 20):self.message = "You Are God!";break;
+			case (count >= 1  && count <  3):self.message = win_text1[self.randomNumber(win_text1.length)];break;
+			case (count >= 3  && count <  6):self.message = win_text2[self.randomNumber(win_text2.length)];break;
+			case (count >= 6  && count <  9):self.message = win_text3[self.randomNumber(win_text3.length)];break;
+			case (count >= 9  && count < 12):self.message = win_text4[self.randomNumber(win_text4.length)];break;
+			case (count >= 12 && count < 16):self.message = win_text5[self.randomNumber(win_text5.length)];break;
+			case (count >= 16):self.message = "You Are God!";break;
 			default:self.message = "Correct!";
 		}
 	}
 	self.loseMessage = function(){
 		switch(true){
-			case (turn == 1):self.message = lose_text1[self.randomNumber(4)];break;
-			case (turn > 1  && turn <  5):self.message = lose_text2[self.randomNumber(4)];break;
-			case (turn >= 5 && turn < 10):self.message = lose_text3[self.randomNumber(4)];break;
-			case (turn >= 10):self.message = "You Were Amazing!";break;
+			case (self.level >= 1  && self.level <  4):self.message = lose_text1[self.randomNumber(lose_text1.length)];break;
+			case (self.level >= 4  && self.level <  7):self.message = lose_text2[self.randomNumber(lose_text2.length)];break;
+			case (self.level >= 7  && self.level < 10):self.message = lose_text3[self.randomNumber(lose_text3.length)];break;
+			case (self.level >= 10 && self.level < 16):self.message = lose_text4[self.randomNumber(lose_text4.length)];break;
+			case (self.level >= 16):self.message = "Even God Can Lose";break;
 			default:self.message = "Game Over Man";
 		} 
 	}
 	self.randomNumber = function(num){return Math.floor(Math.random()*num);}
 	self.scoreCount = function(){
 		switch(true){
-			case (turn <= 4):self.score++;break;
-			case (turn >=5  && turn <  8):self.score+=10;break;
-			case (turn >=8  && turn < 10):self.score+=50;break;
-			case (turn >=10 && turn < 15):self.score+=100;break;
-			case (turn >=15 && turn < 20):self.score+=500;break;
-			case (turn >=20):self.score+=1000;break;
+			case (self.level <= 3):self.score++;break;
+			case (self.level >= 3 && self.level <  6):self.score+=7;break;
+			case (self.level >= 6 && self.level <  9):self.score+=99;break;
+			case (self.level >= 9 && self.level < 12):self.score+=326;break;
+			case (self.level >=12 && self.level < 15):self.score+=1239;break;
+			case (self.level >=15 && self.level < 20):self.score+=5898;break;
+			case (self.level >=20):self.score+=9001;break;
 			default:self.score+=7;
 		} 
 	}
+	self.scoreView = function(){
+		if(self.score_clicked==true){
+			self.score_clicked=false;
+		}else{
+			self.score_clicked=true;
+		}
+	}
 	self.gameOver = function(){
 		self.loseMessage();
+		// self.newHighScore();
 		$timeout(function(){self.colorClicked(self.tetra_choices[count]);}, timer*1.5);
 		$timeout(function(){self.colorClicked(self.tetra_choices[count]);}, timer*3);
 		$timeout(function(){self.message = "Try Again";}, timer*4.5);
 		$timeout(function(){self.reset();}, timer*7.5);
 	}
+	// self.newHighScore = function(){
+		// if(self.score_board.length == 0){
+			// self.score_board.push({name: 'Creator', score: self.score});
+		// }
+	// }
+	self.lightClicked = function(){
+		if(self.light==true){
+			self.light=false;
+			document.body.style.background = "#333"; 
+		}else{
+			self.light=true;
+			document.body.style.background = "whitesmoke";
+		}
+	}
 });
+
+
+
+
+
+
+
+
